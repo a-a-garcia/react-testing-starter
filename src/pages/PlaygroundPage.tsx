94de106c7033da66d564d5322187d381a7b5a142
Anthony <angarce01@gmail.com>
2024-10-25 11:47:31 -0700

    Testing SearchBox

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,11 +1,13 @@
 import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
+import SearchBox from "../components/SearchBox";
 import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
   // return <Onboarding />;
   // return <TermsAndConditions />
-  return <ExpandableText text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ut doloribus sapiente molestias quia, reprehenderit ipsum. Animi sapiente quaerat assumenda. Distinctio porro magni nisi praesentium dolores quidem nesciunt repellendus ullam incidunt, voluptate repellat inventore sunt modi? Repudiandae impedit, labore repellat ducimus, reprehenderit quidem facilis praesentium nostrum consectetur velit aliquid sapiente!"} />
+  // return <ExpandableText text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ut doloribus sapiente molestias quia, reprehenderit ipsum. Animi sapiente quaerat assumenda. Distinctio porro magni nisi praesentium dolores quidem nesciunt repellendus ullam incidunt, voluptate repellat inventore sunt modi? Repudiandae impedit, labore repellat ducimus, reprehenderit quidem facilis praesentium nostrum consectetur velit aliquid sapiente!"} />
+  return <SearchBox onChange={(text) => console.log(text)}/>
 };
 
 export default PlaygroundPage;
---
+++ tests/components/SearchBox.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from '@testing-library/react'
+import SearchBox from '../../src/components/SearchBox'
+import userEvent from '@testing-library/user-event'
+vi.mock('../src/components/SearchBox')
+
+describe('SearchBox', () => {
+    const renderComponent = () => {
+        const onChange = vi.fn();
+        onChange.mockImplementation((event) => {
+            if (event.key === 'Enter') {
+            console.log(event.target.value);
+            }
+        });
+        render(<SearchBox onChange={onChange}/>)
+
+        return {
+            //similar to getByRole, but it also confirms that the element has the correct placeholder text
+            input: screen.getByPlaceholderText(/search/i),
+            onChange,
+            user: userEvent.setup()
+        }
+    }
+
+    //test rendering
+    it('should render the input field for searching', () => {
+        //create a mock function
+        const { input } = renderComponent();
+
+        //similar to getByRole, but it also confirms that the element has the correct placeholder text
+        expect(input).toBeInTheDocument();
+    })
+
+    //test implementation
+    it('should call the onChange callback when Enter is pressed', async () => {
+        const { input, onChange, user } = renderComponent();
+        
+        //simulate user typing in the input field
+        const searchTerm = 'test';
+            // this is the syntax for typing in the input field and pressing enter
+            // remember user events are asynchronous
+        await user.type(input, searchTerm + '{enter}');
+        expect(onChange).toHaveBeenCalledWith(searchTerm);
+    })
+
+    it('should not call the onChange callback when Enter is if input field is empty', async () => {
+        const { input, onChange, user } = renderComponent();
+        
+        //simulate user typing in the input field
+            // this is the syntax for typing in the input field and pressing enter
+            // remember user events are asynchronous
+        await user.type(input, '{enter}');
+        expect(onChange).not.toHaveBeenCalledWith();
+    })
+})
