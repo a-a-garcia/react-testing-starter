63d052409f300ef3a34e7a41f067a223359a44d0
Anthony <angarce01@gmail.com>
2024-10-29 16:56:26 -0700

    Mocking the Authentication Status

--- tests/setup.ts
+++ tests/setup.ts
@@ -6,6 +6,10 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
+// remember when we call the mock option, all exports in this module will be replaced with mock functions
+// the reason we are mocking in setup, is that we have several components that use the useAuth0 hook, and we don't want to mock it in every test file
+vi.mock('@auth0/auth0-react')
+
 // this solves a "matchmedia not found" error
 // this is required because our tests are run in a Node environment, simulated in a JSDOM environment. It doesn't have a window object the matchMedia property, so we need to mock it.
 Object.defineProperty(window, "matchMedia", {
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,5 +1,7 @@
 import { delay, http, HttpResponse } from "msw";
 import { server } from "./mocks/server";
+import { User } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const simulateDelay = (endpoint: string) => {
   server.use(
@@ -13,3 +15,24 @@ export const simulateDelay = (endpoint: string) => {
 export const simulateError = (endpoint: string) => {
   server.use(http.get(endpoint, () => HttpResponse.error()));
 };
+
+type AuthState = {
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  user: User | undefined;
+}
+
+export const mockAuthState = (authState: AuthState) => {
+  vi.mocked(useAuth0).mockReturnValue({
+    ...authState,
+    // cmd+. to add all missing functions
+    // we aren't using any of these functions so we can just mock them
+    getAccessTokenSilently: vi.fn().mockResolvedValue("a"),
+    getAccessTokenWithPopup: vi.fn(),
+    getIdTokenClaims: vi.fn(),
+    loginWithPopup: vi.fn(),
+    loginWithRedirect: vi.fn(),
+    logout: vi.fn(),
+    handleRedirectCallback: vi.fn()
+  })
+}
