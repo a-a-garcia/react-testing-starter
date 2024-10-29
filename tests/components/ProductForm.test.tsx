5f1b0c2f5870311de9a81d0ea943d27fd6186af0
Anthony <angarce01@gmail.com>
2024-10-28 18:25:35 -0700

    Exercise - Testing Initial Data

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -1,24 +1,73 @@
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
-import ProductForm from '../../src/components/ProductForm'
-import AllProviders from '../AllProviders'
+import { render, screen } from "@testing-library/react";
+import ProductForm from "../../src/components/ProductForm";
+import { Category, Product } from "../../src/entities";
+import AllProviders from "../AllProviders";
+import { db } from "../mocks/db";
 
-describe('ProductForm', () => {
-    it('should render form fields', async () => {
-        render(<ProductForm onSubmit={vi.fn()}/>, { wrapper: AllProviders })
+describe("ProductForm", () => {
+  let category: Category;
 
-        // we don't want to use different methods to assert, so we can 1) use find to wait for the form to be rendered
-        await screen.findByRole('form')
+  beforeAll(() => {
+    category = db.category.create();
+  });
 
-        // or 2) wait for the loading indicator to be removed
-        // await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+  afterAll(() => {
+    db.category.delete({
+      where: {
+        id: {
+          equals: category.id,
+        },
+      },
+    });
+  });
 
-        //assert name field is in the document
-        expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument()
+  it("should render form fields", async () => {
+    render(<ProductForm onSubmit={vi.fn()} />, { wrapper: AllProviders });
 
-        //we don't need to use a find method for the second field since once the first finishes, our form is already rendered
-        expect(screen.getByPlaceholderText(/price/i)).toBeInTheDocument()
+    // we don't want to use different methods to assert, so we can 1) use find to wait for the form to be rendered
+    await screen.findByRole("form");
 
-        //dropdown assertion
-        expect(screen.getByRole('combobox', { name: /category/i })).toBeInTheDocument()
-    })
-})
+    // or 2) wait for the loading indicator to be removed
+    // await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+
+    //assert name field is in the document
+    expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument();
+
+    //we don't need to use a find method for the second field since once the first finishes, our form is already rendered
+    expect(screen.getByPlaceholderText(/price/i)).toBeInTheDocument();
+
+    //dropdown assertion
+    expect(
+      screen.getByRole("combobox", { name: /category/i })
+    ).toBeInTheDocument();
+  });
+
+  it("should populate form fields when editing a product", async () => {
+    const product: Product = {
+      id: 1,
+      name: "Bread",
+      price: 4.99,
+      categoryId: category.id,
+    };
+
+    render(<ProductForm product={product} onSubmit={vi.fn()} />, { wrapper: AllProviders });
+
+    // we don't want to use different methods to assert, so we can 1) use find to wait for the form to be rendered
+    await screen.findByRole("form");
+
+    // or 2) wait for the loading indicator to be removed
+    // await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+
+    //assert name field is in the document
+    expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name);
+
+    //we don't need to use a find method for the second field since once the first finishes, our form is already rendered
+    // must convert product.price to string because value property of a form field is always a string
+    expect(screen.getByPlaceholderText(/price/i)).toHaveValue(product.price.toString());
+
+    //dropdown assertion
+    expect(
+      screen.getByRole("combobox", { name: /category/i })
+    ).toHaveTextContent(category.name);
+  });
+});
